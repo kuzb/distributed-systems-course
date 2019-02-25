b487c3e4ed1723f4075939e3eeb95c17b80150bb
Burak Kuz <kuz.burak.96@gmail.com>
2019-02-25 11:58:28 -0500

    now uses ns instead of the uri

--- LAB1/client.py
+++ LAB1/client.py
@@ -1,8 +1,10 @@
 import Pyro4
+# use this if you want to use uri
+#uri = input("what is the URI of the greeting object?").strip()
+#server_ref=Pyro4.Proxy(uri)
 
-uri = input("what is the URI of the greeting object?").strip()
-server_ref=Pyro4.Proxy(uri)
-
+# use this in order to use nameserver
+server_ref = Pyro4.Proxy("PYRONAME:example.greeting")
 name = input("what is your name?").strip()
 response=server_ref.get_fortune(name)
 print(response)
--- LAB1/server.py
+++ LAB1/server.py
@@ -5,14 +5,17 @@ import random
 class GreetingMaker(object):
     def get_fortune(self,name):
         print(name + " is calling the method")
-        num = random.randint(0, 10**6)
+        num = random.randint(0, 10)
         return ("Hello, " + name + " your lucky number is " + str(num))
 
 deamon = Pyro4.Daemon()
+ns = Pyro4.locateNS()
 uri = deamon.register(GreetingMaker)
 
+# Instead connecting using the uri, we are using ns to connect using "example.greeting" tag
+ns.register("example.greeting", uri)
+# you need to run "python -m Pyro4.naming" beforing starting the server
 
-print(uri)
 
 print("READY")
 deamon.requestLoop()
