23e76e7dcf196b6c18f3a71d6e071f2721529fff
Burak Kuz <kuz.burak.96@gmail.com>
2019-02-27 05:09:12 -0500

    removed the unnecessery file, and *_uri version are updated to use nameserver

--- LAB1/client.py
+++ LAB1/client.py
@@ -2,27 +2,25 @@ import Pyro4
 import subprocess
 import os
 
-#restore the terminal to its original operating mode.
-def handleInterrupt():
-        try:
-            subprocess.call(["clear"])
-            sys.exit(0)
-        except SystemExit:
-            subprocess.call(["clear"])
-            os._exit(0)
 
-# use this if you want to use uri
-#uri = input("what is the URI of the greeting object?").strip()
-#server_ref=Pyro4.Proxy(uri)
 
-# use this in order to use nameserver
-server_ref = Pyro4.Proxy("PYRONAME:example.greeting")
-name = input("what is your name?").strip()
-response=server_ref.get_fortune(name)
-print(response)
+def terminate(remote):
+    remote.shutdown()
+    remote._pyroRelease()
 
 if __name__ == '__main__':
     try:
-        matrix = Rain()
+        # uses nameserver inorder to connect the server
+        remote = Pyro4.Proxy("PYRONAME:example.sum")
+        
+        int1 = input("Give the first number: ")
+        int2 = input("Give the second number: ")
+
+        response = remote.print_sum(int1, int2)
+
+        print("Response is: " + str(response))
+
+        terminate(remote)
     except KeyboardInterrupt: # To handle keyboardinterrupt more gracefully
-        handleInterrupt()
+        terminate(remote)
+        print("Terminated")
--- LAB1/server.py
+++ LAB1/server.py
@@ -6,34 +6,28 @@ import sys
 
 @Pyro4.expose
 class GreetingMaker(object):
-    def get_fortune(self,name):
-        print(name + " is calling the method")
-        num = random.randint(0, 10)
-        return ("Hello, " + name + " your lucky number is " + str(num))
     def print_sum(self, int1, int2):
-        #print("Going to add following intergers" + str(int1) + " and " + str(int2))
+        print("Client asked for the sum of " + str(int1) + " and " + str(int2))
         num = int(int1) + int(int2)
         return num
 
-
-def main():
-    daemon = Pyro4.Daemon()
-    ns = Pyro4.locateNS()
-    uri = daemon.register(GreetingMaker)
-
-    # Instead connecting using the uri, we are using ns to connect using "example.greeting" tag
-    ns.register("example.greeting", uri)
-    # you need to run "python -m Pyro4.naming" beforing starting the server
-
-
-    print("READY")
-    daemon.requestLoop()
-
-
 if __name__ == '__main__':
     try:
-        subp = subprocess.Popen(['python -m Pyro4.naming'])
-        main()
+
+        daemon = Pyro4.Daemon()
+        
+        # uses nameserver in order expose endpoint
+        ns = Pyro4.locateNS()
+        uri = daemon.register(GreetingMaker)
+    
+        # you need to run "python -m Pyro4.naming" beforing starting the server
+        ns.register("example.sum", uri)
+        
+    
+    
+        print("READY")
+        daemon.requestLoop()
+
     except KeyboardInterrupt: # To handle keyboardinterrupt more gracefully
-        subp.kill()
+        daemon.shutdown()
         print("NS is shut down and program is terminated")
