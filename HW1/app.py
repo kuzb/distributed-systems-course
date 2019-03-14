cd1a333edd412bb57d666af87d0134a29d90fb44
Burak Kuz <kuz.burak.96@gmail.com>
2019-03-14 16:20:10 -0400

    classes doesn't have side effect

--- HW1/app.py
+++ HW1/app.py
@@ -26,11 +26,24 @@ class seat:
     def chooseSeat(self, seatNumber, pnr):
         self.chooseSeat[seatNumber] = pnr
 
+    def getSeatNumber(self, pnr):
+        for seat in self.seatsAvaible:
+            if seat == pnr:
+                return seat
+        return False
+
     def cancelReserveSeat(self):
         self.numberOfSeatAvaible += 1
 
     def cancelChooseSeat(self, seatNumber):
         self.chooseSeat[seatNumber] = 0
+
+    def cancelChooseSeatByPNR(self, pnr):
+        for seat in self.seatsAvaible:
+            if seat == pnr:
+                seat = 0
+                return True
+        return False
 
 class flights:
     def __init__(self):
@@ -77,44 +90,7 @@ class flights:
             if(id == a_flight["general"]["flight_id"]):
                 return a_flight["seats"]
 
-
-    # didn't book the seat(position) yet
-    # returns: boolFlightExists, boolSeatAvailable
-
-    def registerTicket(self, id): 
-        for a_flight in self.all_flights:
-            if(id == a_flight["flight_id"]):
-                if a_flight["seats"] < 100:
-                    a_flight["seats"] = a_flight["seats"] + 1
-                    return True, True # Flight exits and there is space
-                return True, False # Flight exists but no space
-        return False, False # Flight doesn't exists, all false
-
-    def reserveSeat(self, id, seatNumber):
-        for a_flight in self.all_flights:
-            if(id == a_flight["flight_id"]):
-                if a_flight["seat_available"][seatNumber]:
-                    # Seat is reserved
-                    a_flight["seat_available"][seatNumber] = False
-                    return True
-                return False # Seat is not available
-        return False  # Flight doesn't exists
-
-    def cancelSeat(self, id, seatNumber):
-        for a_flight in self.all_flights:
-            if(id == a_flight["flight_id"]):
-                if not (a_flight["seat_available"][seatNumber]):
-                    a_flight["seat_available"][seatNumber] = True
-                    return True # Seat is again available
-                return False # Seat was already available
-        return False  # Flight doesn't exists
-    
-    def cancelTicket(self, id):
-        for a_flight in self.all_flights:
-            if(id == a_flight["flight_id"]):
-                a_flight["seats"] = a_flight["seats"] - 1
-                return True # Flight exits and one more seat available
-        return False # Flight doesn't exists 
+  
 
 the_flights = flights()
 
@@ -137,64 +113,63 @@ class ticket:
         "flight_id" : flight_id
         })
 
-
     def get(self, pnr):
-        for ticket in self.all_tickets:
-            if int(pnr) == ticket["PNR"]:
-                return ticket
-        return jsonify({"PNR" : pnr}) ,404
-
-    def chooseSeat(self,pnr, seat_id):
-        for ticket in self.all_tickets:
-            if int(pnr) == ticket["PNR"]:
-                if the_flights.reserveSeat(ticket["flight_id"],seat_id-1):
-                    ticket["seat_number"] = seat_id
-                    return jsonify({
-                        "PNR" : ticket["PNR"],
-                        "seat_number" : seat_id
-                    }), 200 # seat number is available
-                return 409 # seat number is not available
-        return 404 # PNR does not exist
-    
-    def getAll(self):
-        temp = []
-        for ticket in self.all_tickets:
-            temp.append({
-                "dest" : the_flights.get(ticket["flight_id"])["dest"],
-                "from" : the_flights.get(ticket["flight_id"])["from"],
-                "date" : the_flights.get(ticket["flight_id"])["date"],
-                "flight_id" : ticket["flight_id"],
-                "PNR" : ticket["PNR"]
-            })
-        return jsonify(temp), 200
+        for a_ticket in self.all_tickets:
+            if int(pnr) == a_ticket["PNR"]:
+                return a_ticket
+        return False
 
     def delete(self, pnr):
-        for ticket in self.all_tickets:
-            if int(pnr) == ticket["PNR"]:
-                the_flights.cancelSeat(ticket["flight_id"], ticket["seat_number"] - 1)
-                the_flights.cancelTicket(ticket["flight_id"])
-                self.all_tickets.remove(ticket)
-                return jsonify({"PNR" : pnr}), 200
-        return jsonify({"PNR" : pnr}), 400
+        for a_ticket in self.all_tickets:
+            if int(pnr) == a_ticket["PNR"]:
+                self.all_tickets.remove(a_ticket)
+                return True
+        return False
+
+    def isExist(self, pnr):
+        for a_ticket in self.all_tickets:
+            if int(pnr) == a_ticket["PNR"]:
+                return True
+        return False
 
 the_tickets = ticket()
 
 class Ticket(Resource):
-    def put(self):
+    def put(self): # buying a ticket for a given flight
         global _PNR_ID
         _PNR_ID += 1
         parser = reqparse.RequestParser()
         parser.add_argument("flight_id")
         args = parser.parse_args()
-        return Response(the_tickets.add(int(args["flight_id"])))
+        if the_flights.isExist(int(args["flight_id"])):
+            if the_flights.getSeatObject(int(args["flight_id"])).isFull():
+                the_flights.getSeatObject(int(args["flight_id"])).reserveSeat()
+                the_tickets.add(int(args["flight_id"]), _PNR_ID)
+                return {
+                    "PNR" : _PNR_ID
+                }, 200
+            return 409
+        return 404
 
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("PNR")
         args = parser.parse_args()
         if args["PNR"]:
-            return the_tickets.get(int(args["PNR"]))
-        return Response(the_tickets.getAll())
+            if the_tickets.isExist(int(args["PNR"])):
+                ticket = the_tickets.get(int(args["PNR"]))
+                flight_id = ticket["flight_id"]
+                flight = the_flights.get(flight_id)
+                seat = the_flights.getSeatObject(flight_id)
+                return {
+                    "dest" : flight["dest"], 
+                    "from" : flight["from"],
+                    "date" : flight["date"],
+                    "flight_id" : flight_id,
+                    "seat_number" : seat.getSeatNumber(int(args["PNR"]))
+                }, 200
+            return 404
+        # GetAll
         
 
     def post(self):
@@ -202,14 +177,26 @@ class Ticket(Resource):
         parser.add_argument("PNR")
         parser.add_argument("seat_number")
         args = parser.parse_args()        
-        return Response(the_tickets.chooseSeat(int(args["PNR"]),int(args["seat_number"])))
+        if the_tickets.isExist(int(args["PNR"])):    
+            flight_id = the_tickets.get("PNR")["flight_id"]   
+            seat_obj =the_flights.getSeatObject(flight_id) 
+            if seat_obj.isSeatEmpty(args["seat_number"]):
+                return 200
+            return 409
+        return 404
     
     def delete(self):
         parser = reqparse.RequestParser()
         parser.add_argument("PNR")
         args = parser.parse_args()        
-        return Response(the_tickets.delete(int(args["PNR"])))
-
+        if the_tickets.isExist(int(args["PNR"])):  
+            flight_id = the_tickets.get(int(args["PNR"]))["flight_id"]  
+            the_tickets.delete(int(args["PNR"]))
+            seat_obj = the_flights.getSeatObject(flight_id)
+            seat_obj.cancelChooseSeatByPNR(int(args["PNR"]))
+            seat_obj.cancelReserveSeat()
+            return 200
+        return 404     
 
 # RESTFUL
 class Flight(Resource):
